674ea755ebe1c93ed82adb7463e13065d64e12eb
ooniwatori <94368724+ooniwatori@users.noreply.github.com>
2024-05-19 17:15:18 +0900

    Update ImageFFT.java
    Add comments

--- src/application/ImageFFT.java
+++ src/application/ImageFFT.java
@@ -3,8 +3,9 @@ import java.awt.image.BufferedImage;
 
 import FFT.*;
 import java.awt.Color;
-
+/* Implement 2DFFT for Image data */
 public class ImageFFT {
+	/* Define height ,width, original height, original width, image datas stored by matrix, BufferedImage datas */
 	private int h;
 	private int w;
 	private int hOrigin;
@@ -17,7 +18,7 @@ public class ImageFFT {
 	private BufferedImage imageFFT1P;
 	private BufferedImage imageFFT2M;
 	private BufferedImage imageFFT2P;
-	
+	/* Filter matrix and processed matrix, BufferedImage datas*/
 	private Complex[][] filter;
 	private Complex[][] processedFFT2Matrix;
 	private Complex[][] processedFFT1Matrix;
@@ -26,24 +27,29 @@ public class ImageFFT {
 	private BufferedImage processedImage;
 	private BufferedImage filterImage;
 	
-	
+	/* Do not use this Constructor */
 	public ImageFFT() {
 		
 	}
-	
+	/* Constructor initialized by initial image data */
 	public ImageFFT(BufferedImage image) {
+		/* Pass each property */
 		originalImage = image;
 		hOrigin = image.getHeight();
 		wOrigin = image.getWidth();
+		/* FFT only process data with length of 2^k, so normalize length */
 		h = (int)Math.pow(2, get2Pow(hOrigin));
 		w = (int)Math.pow(2, get2Pow(wOrigin));
+		/* Make matrix data to a square */
 		if(h > w) {
 			w = h;
 		}
 		else {
 			h = w;
 		}
+		/* Initialize all sizes of matrix and image datas*/
 		init();
+		/* 2DFFT only can process gray scale image, here use red scale to process */
 		for(int i = 0; i < h; ++i) {
 			for(int j = 0; j < w; ++j) {
 				try {
@@ -54,12 +60,14 @@ public class ImageFFT {
 				}
 			}
 		}
+		/* 2DFFT */
 		fft2d();
 		imageTransform();
 	}
-	
+	/* Initialize filter matrix, use omega0, omega1 for parameter, type for fileter type*/
 	public void filterDesign(int omega0, int omega1, int type) {
 		switch(type) {
+		/* Low pass filter, implement by circle with value 1 pixel */
 		case 1:
 			for(int i = -h/2; i < h/2; ++i) {
 				for(int j = -w/2; j < w/2; ++j) {
@@ -70,6 +78,7 @@ public class ImageFFT {
 				}
 			}
 			break;
+		/* High pass filter, implement by donuts with value 1 pixel*/
 		case 2:
 			for(int i = -h/2; i < h/2; ++i) {
 				for(int j = -w/2; j < w/2; ++j) {
@@ -80,6 +89,7 @@ public class ImageFFT {
 				}
 			}
 			break;
+		/* Band pass filter */
 		case 3:
 			for(int i = -h/2; i < h/2; ++i) {
 				for(int j = -w/2; j < w/2; ++j) {
@@ -91,6 +101,7 @@ public class ImageFFT {
 				}
 			}
 			break;
+		/* Band reject filter*/
 		case 4:
 			for(int i = -h/2; i < h/2; ++i) {
 				for(int j = -w/2; j < w/2; ++j) {
@@ -105,7 +116,7 @@ public class ImageFFT {
 		}
 	}
 	
-	
+	/* Filter applying, by multiplication image data and filter in frequecy domain */
 	public void filterApply(int omega0, int omega1, int type) {
 		filterDesign(omega0, omega1, type);
 		for(int i = 0; i < h; ++i) {
@@ -114,16 +125,18 @@ public class ImageFFT {
 				
 			}
 		}
+		/* Inverse 2DFFT on image data*/
 		ifft2d();
 		inverseImageTransform();
 	}
 	
 	
 	
-	
+	/* log2(n) */
 	public int get2Pow(int n) {
 		return (int)Math.ceil(Math.log(n)/Math.log(2));
 	}
+	/* Initial all matrix and Image datas to adjustde size */
 	public void init() {
 		originalImageMatrix = new Complex[h][w];
 		imageFFT1Matrix = new Complex[h][w];
@@ -144,20 +157,23 @@ public class ImageFFT {
 		filterImage = new BufferedImage(w, h, BufferedImage.TYPE_BYTE_GRAY);
 		
 	}
+	/* Get red scale of image */
 	public void decodeRGB(Color c, int i, int j) {
 		originalImageMatrix[i][j] = new Complex(c.getRed(), 0);
 	}
 	
-	
+	/* 2DFFT: do FFT on row data then do FFT on column data */
 	public void fft2d() {
 		fft1();
 		fft2();
 	}
+	/* FFT row data */
 	public void fft1() {
 		for(int i = 0; i < h; ++i) {
 			imageFFT1Matrix[i] = FFT.fft(originalImageMatrix[i]);
 		}
 	}
+	/* FFT column data */
 	public void fft2() {
 		Complex[][] Temp = transp(imageFFT1Matrix, h, w);
 		for(int i = 0; i < h; ++i) {
@@ -166,16 +182,18 @@ public class ImageFFT {
 		imageFFT2Matrix = transp(imageFFT2Matrix, h, w);
 		imageFFT2Matrix = FFT.fftshift(imageFFT2Matrix);
 	}
-	
+	/* Inverse 2DFFT*/
 	public void ifft2d() {
 		ifft2();
 		ifft1();
 	}
+	/* Inverse FFT row data */
 	public void ifft1() {
 		for(int i = 0; i < h; ++i) {
 			processedMatrix[i] = FFT.ifft(processedFFT1Matrix[i]);
 		}
 	}
+	/* Inverse FFT column data */
 	public void ifft2() {
 		processedFFT2Matrix = FFT.fftshift(processedFFT2Matrix);
 		Complex[][] Temp = transp(processedFFT2Matrix, h, w);
@@ -185,10 +203,11 @@ public class ImageFFT {
 		processedFFT1Matrix = transp(processedFFT1Matrix, h, w);
 		
 	}
+	/* Recreate a complex matrix of image matrix for analyzing */
 	public Complex[][] transp(Complex[][] m, int h, int w) {
 		Complex[][] c = new Complex[h][w];
-		for(int i = 0; i < w; ++i) { // i: 1024
-			for(int j = 0; j < h; ++j) { // j: 512
+		for(int i = 0; i < w; ++i) { 
+			for(int j = 0; j < h; ++j) { 
 				c[i][j] = new Complex(m[j][i].getReal(), m[j][i].getImage());
 			}
 		}
@@ -211,7 +230,7 @@ public class ImageFFT {
 		}
 	}
 	
-	
+	/* For human visual, adjust pixels' value in a multipier scale, here also process the filter image */
 	public void inverseImageTransform() {
 		processedFFT2Matrix = FFT.fftshift(processedFFT2Matrix);
 		for(int i = 0; i < h; ++i) {
@@ -230,8 +249,9 @@ public class ImageFFT {
 			}
 		}
 	}
-	
+	/* For human visual, adjust pixels' value in a multipier scale, here only process image data */
 	public void inverseImageOnlyTrans() {
+		/* Shifting first */
 		processedFFT2Matrix = FFT.fftshift(processedFFT2Matrix);
 		for(int i = 0; i < h; ++i) {
 			for(int j = 0; j < w; ++j) {
@@ -240,6 +260,7 @@ public class ImageFFT {
 				processedFFT2.setRGB(j, i, new Color(r2m, r2m, r2m).getRGB());	
 			}
 		}
+		/* Extract the magnitude value of each pixel */
 		for(int i = 0; i < hOrigin; ++i) {
 			for(int j = 0; j < wOrigin; ++j) {
 				int rm = bound((int)Math.round(processedMatrix[i][j].magnitude()));
@@ -248,7 +269,7 @@ public class ImageFFT {
 		}
 	}
 	
-	
+	/* Testing, applying phase on different image, not be used */
 	public void applyPhaseMix(double[][] angle) {
 		for(int i = 0; i < h; ++i) {
 			for(int j = 0; j < w; ++j) {
@@ -259,7 +280,7 @@ public class ImageFFT {
 		ifft2d();
 		inverseImageOnlyTrans();
 	}
-	
+	/* Limit pixel scale in 255 */
 	public int bound(int n) {
 		if(n > 255)
 			return 255;
@@ -268,10 +289,12 @@ public class ImageFFT {
 		else
 			return n;
 	}
+	/* Limit angle in pi */
 	public int angleBound(int n) {
 		return (int)Math.round(Math.abs(n%Math.PI));
 	}
 
+	/* Getter for Bufferimage data */
 	public BufferedImage getImageFFT1Magnitude() {
 		return imageFFT1M;
 	}
@@ -297,6 +320,8 @@ public class ImageFFT {
 	public BufferedImage getFilter() {
 		return filterImage;
 	}
+
+	/* Get phase value of image matrix */
 	public double[][] getPhase(){
 		double[][] angle = new double[h][w];
 		for(int i = 0; i < h; ++i) {
